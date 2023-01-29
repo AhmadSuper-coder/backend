6a02e567b7f57268e9e05f0216a63bf352e00e82
Ahmad Raja <ahmdraza001912@gmail.com>
2023-01-30 02:18:58 +0530

    third commit

--- database/db.js
+++ database/db.js
@@ -1,8 +1,8 @@
 const mongoose=require("mongoose");
 
 mongoose.set("strictQuery", false);
-const url="mongodb+srv://ahmad:Q5LMnwUMWcBnH1q1@cluster0.ebgog9n.mongodb.net/crudapp";
-mongoose.connect(url).then(
+const url="mongodb+srv://ahmad:Sonu001912@cluster0.ebgog9n.mongodb.net/crudapp";
+mongoose.connect(url,{ useUnifiedTopology: true, useNewUrlParser: true }).then(
     ()=>{
         console.log("Database has been statblish")
     }
